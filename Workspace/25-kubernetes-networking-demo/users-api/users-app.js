3ee71ec7658feab5cde10cb5c627934a35a61f66
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-05-21 15:05:01 +0530

    Pod-to-Pod comminucation - using direct IP address and better way of using kubernetes generated ENV variables

--- Workspace/25-kubernetes-networking-demo/users-api/users-app.js
+++ Workspace/25-kubernetes-networking-demo/users-api/users-app.js
@@ -11,28 +11,22 @@ app.post('/signup', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (
-    !password ||
-    password.trim().length === 0 ||
-    !email ||
-    email.trim().length === 0
-  ) {
-    return res
-      .status(422)
-      .json({ message: 'An email and password needs to be specified!' });
+  if (!password || password.trim().length === 0 || !email || email.trim().length === 0) {
+    return res.status(422).json({ message: 'An email and password needs to be specified!' });
   }
 
   try {
-    const hashedPW = await axios.get(`http://${process.env.AUTH_ADDRESS}/hashed-password/` + password);
+    // way 2 - pod-to-pod communication using Kubernetes generated env variable
+    const hashedPW = await axios.get(
+      `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/hashed-password/` + password
+    );
     // const hashedPW = 'dummy text';
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
     res.status(201).json({ message: 'User created!' });
   } catch (err) {
     console.log(err);
-    return res
-      .status(500)
-      .json({ message: 'Creating the user failed - please try again later.' });
+    return res.status(500).json({ message: 'Creating the user failed - please try again later.' });
   }
 });
 
@@ -41,21 +35,15 @@ app.post('/login', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (
-    !password ||
-    password.trim().length === 0 ||
-    !email ||
-    email.trim().length === 0
-  ) {
-    return res
-      .status(422)
-      .json({ message: 'An email and password needs to be specified!' });
+  if (!password || password.trim().length === 0 || !email || email.trim().length === 0) {
+    return res.status(422).json({ message: 'An email and password needs to be specified!' });
   }
 
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
+  // way 2 - pod-to-pod communication using Kubernetes generated env variable
   const response = await axios.get(
-    `http://${process.env.AUTH_ADDRESS}/token/` + hashedPassword + '/' + password
+    `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/token/` + hashedPassword + '/' + password
   );
   // const response = { status: 200, data: { token: 'abc' } };
   if (response.status === 200) {
